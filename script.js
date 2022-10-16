f2ccbe2cc734932379e72b310c0e268f08d24261
Ahmed Ramic <ahmedramic@Ahmeds-MacBook-Air.local>
2022-10-16 19:57:33 -0400

    Created a function to prompt the user to enter the length of the password. Then created another function to generate the password. Also changed the vars to const or let.

--- script.js
+++ script.js
@@ -7,18 +7,31 @@ const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "123456789";
 const specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
+//Going to make my first function to determine length of password
+function calculatePasswordLength(){
+  let passwordLength;
+  passwordLength = prompt("Please enter the length of your password (Must be between 8 and 128 characters)");
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Invalid password length. Must be a numeric value between 8 and 128 characters");
+    calculatePasswordLength();
+  }else {
+    alert("Your password length has been determined. Please move on to the next step");
+  }
+return passwordLength;
+}
 
-
+function generatePassword(){
+  calculatePasswordLength();
+  console.log(passwordLength);
+}
 
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password")
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
