f6cf133e84ca416385466e2faf6487582e5122db
Ahmed Ramic <ahmedramic@Ahmeds-MacBook-Air.local>
2022-10-17 17:29:08 -0400

    I created an iteration which in theory should have gone through all the true statements and added it to the password but it wasnt storing the value correctly. So then I used the (Innocent until proven guilty approach) and defined everything as true. If the user did not want something then by typying noit should have skipped and moved onto the next function. Then the password would get generated based off all the true statements.

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "123456789";
 const specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-let passwordLength;
-let upperCaseRule;
-let numbersRule;
-let specialsRule;
+let passwordLength = "";
+let upperCaseRule = true;
+let lowerCaseRule = true;
+let numbersRule = true;
+let specialsRule = true;
+let selections;
 
 //Going to make my first function to determine length of password
 function calculatePasswordLength(){
@@ -20,27 +22,46 @@ function calculatePasswordLength(){
     alert("Invalid password length. Must be a numeric value between 8 and 128 characters");
     calculatePasswordLength();
   }else {
-    alert("Your password length has been determined. Please move on to the next step");
+    alert("Your password length has been determined. Please move on to the next few steps");
   }
 return passwordLength;
 }
+console.log(passwordLength);
 
 
 //Creating another function to see if the user wants uppercase letters
-function dictateUpperCase() {
-  let upperCaseRule = prompt("Would you like to include uppercase letters? Please type:(Yes or No)");
-  if (upperCaseRule === "yes"){
-    upperCaseRule = true;
-    return upperCaseRule;
-  } else if (upperCaseRule === "no"){
-    upperCaseRule = false;
-    return upperCaseRule;
+  function dictateUpperCase() {
+    let upperCaseRule = prompt("Would you like to include uppercase letters? Please type:(Yes or No)");
+    if (upperCaseRule === "yes"){
+      upperCaseRule = true;
+      return upperCaseRule;
+    } else if (upperCaseRule === "no"){
+      upperCaseRule = false;
+      return upperCaseRule;
+    }else {
+      alert("Invalid Answer! Please type:(Yes or No)")
+      dictateUpperCase();
+      }
+      return upperCaseRule;
+    }
+    console.log(upperCaseRule);
+
+//Creating another function to see if the user wants lowercase letters
+function dictateLowerCase() {
+  let lowerCaseRule = prompt("Would you like to include lowercase letters? Please type:(Yes or No)");
+  if (lowerCaseRule === "yes"){
+    lowerCaseRule = true;
+    return lowerCaseRule;
+  } else if (lowerCaseRule === "no"){
+    lowerCaseRule = false;
+    return lowerCaseRule;
   }else {
     alert("Invalid Answer! Please type:(Yes or No)")
-    dictateUpperCase();
+    dictateLowerCase();
     }
-    return upperCaseRule;
+    return lowerCaseRule;
   }
+  console.log(lowerCaseRule);    
 
   //Creating a function to see if the User wants numbers
   function dictateNumbers() {
@@ -57,15 +78,16 @@ function dictateUpperCase() {
       }
       return numbersRule;
     }
+    console.log(numbersRule);
 
     //Creating a function to see if the User wants special characters
   function dictateSpecial() {
     let specialsRule = prompt("Would you like to include special characters? Please type:(Yes or No)");
     if (specialsRule === "yes"){
-      specialsRule = true;
+      specialsRule == true;
       return specialsRule;
     } else if (specialsRule === "no"){
-      specialsRule = false;
+      specialsRule == false;
       return specialsRule;
     }else {
       alert("Invalid Answer! Please type:(Yes or No)")
@@ -73,19 +95,38 @@ function dictateUpperCase() {
       }
       return specialsRule;
     }
+    console.log(specialsRule);
 
-  
+//This is to call the functions
 function generatePassword(){
   calculatePasswordLength();
-  console.log(passwordLength);
   dictateUpperCase();
-  console.log(upperCaseRule);
+  dictateLowerCase();
   dictateNumbers();
-  console.log(numbersRule);
   dictateSpecial();
-  console.log(specialsRule);
+  
+
+let password = "";
+let selections = "";
+
+if (lowerCaseRule){
+  selections += lowerCase;
+}
+if (upperCaseRule){
+  selections += upperCase;
+}
+if (numbersRule){
+  selections += numbers;
+}
+if (specialsRule){
+  selections += specialCharacters;
 }
 
+for(let i = 0; i < passwordLength; i++) {
+  password += selections.charAt(Math.floor(Math.random() * selections.length));
+  }
+  return password;
+}
 
 
 // Write password to the #password input
